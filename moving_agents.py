2f64832b6a9ab6573e6037868d0b1bb8156fc029
Arindam Saha <arindamsaha1507@gmail.com>
2024-03-05 06:26:50 +0000

    Removed unnecessary comments

--- moving_agents.py
+++ moving_agents.py
@@ -1,4 +1,5 @@
-# import pygame
+"""Main file for the moving agents simulation."""
+
 import random
 import math
 import multiprocessing
@@ -11,7 +12,6 @@ NUM_AGENTS = 100
 CIRCLE_SIZE = 50
 
 TIME = 6000
-# TIME = 3 * 24 * 60  # 3 days
 
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
@@ -19,23 +19,12 @@ BLUE = (0, 0, 255)
 
 
 def levy_walk(agent, angle, step):
-    # Generate a step length from a Levy distribution (power-law distribution)
 
-    # beta = movement_scale_factor / (
-    #     movement_scale_factor - 1
-    # )  # Adjust this value for different Levy distributions
-
-    # beta = movement_scale_factor  # Adjust this value for different Levy distributions
-    # beta = 1.5  # Adjust this value for different Levy distributions
-    # step_length = random.paretovariate(beta)
-
-    # Update agent's position based on the new angle and step length
     agent["x"] += step * math.cos(angle)
     agent["y"] += step * math.sin(angle)
 
 
 def random_walk(agent, angle, step):
-    # step_length = movement_scale_factor
 
     if angle < math.pi / 2:
         agent["x"] += step
@@ -48,10 +37,6 @@ def random_walk(agent, angle, step):
 
 
 def brownian_motion(agent, angle, step):
-
-    # step_length = np.random.normal(0, movement_scale_factor * np.sqrt(np.pi / 2))
-
-    # angle += random.uniform(-math.pi / 2, math.pi / 2)
 
     agent["x"] += step * math.cos(angle)
     agent["y"] += step * math.sin(angle)
@@ -83,8 +68,6 @@ def main(mode, movement_scale_factor):
 
     angles = np.random.uniform(0, 2 * math.pi, TIME * NUM_AGENTS)
 
-    # print(f"Distributions generated for scale {movement_scale_factor}. Starting simulation...")
-
     for time_idx in range(TIME):
 
         for agent_idx, agent in enumerate(agents):
@@ -92,18 +75,11 @@ def main(mode, movement_scale_factor):
             index = time_idx * NUM_AGENTS + agent_idx
             # walk
             if mode == 0:
-                # levy_walk(agent, random.uniform(0, 2 * math.pi), movement_scale_factor)
                 levy_walk(agent, angles[index], steps[index])
             elif mode == 1:
-                random_walk(
-                    # agent, random.uniform(0, 2 * math.pi), movement_scale_factor
-                    agent, angles[index], steps[index]
-                )
+                random_walk(agent, angles[index], steps[index])
             else:
-                brownian_motion(
-                    agent, angles[index], steps[index]
-                    # agent, random.uniform(0, 2 * math.pi), movement_scale_factor
-                )
+                brownian_motion(agent, angles[index], steps[index])
 
             # Wrap agents around the screen
             agent["x"] = agent["x"] % WIDTH
